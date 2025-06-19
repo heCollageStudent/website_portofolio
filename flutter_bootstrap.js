df2850e41ad7aa06c1895abb6ad1971c9c9de20a
heCollageStudent <ramadhandhirsya@gmail.com>
2025-06-19 23:31:31 +0700

    deploy for test look portofolio section

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"83082c19b72717dbc6504cf38ce920b402440e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2691747042"
+    serviceWorkerVersion: "4215690644"
   }
 });
